aae730d2878077fa8f9f24e266e807802de2af38
Elliott Buckingham <Bucfringe@gmail.com>
2022-03-28 15:26:36 +0100

    init commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    sm: '640px',
+    md: '768px',
+    lg: '1024px',
+    xl: '1280px',
+    '2xl': '1536px',
+  },
+  plugins: [],
+}
